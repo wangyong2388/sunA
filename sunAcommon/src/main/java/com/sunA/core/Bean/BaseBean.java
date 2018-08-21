2a362cf6d3c78c2f6ebaeb6ab7a224789237e298
ayong <1196334604@qq.com>
2018-08-21 10:20:16 +0800

    test1

---
+++ sunAcommon/src/main/java/com/sunA/core/Bean/BaseBean.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: BaseBean
+ * Author:   ayong
+ * Date:     2018/8/21 10:19
+ * Description: 实体类要继承的类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ */
+package com.sunA.core.Bean;
+
+/**
+ *
+ * 〈实体类要继承的类〉
+ *
+ * @author ayong
+ * @create 2018/8/21
+ * @since 1.0.0
+ */
+public class BaseBean {
+
+}
